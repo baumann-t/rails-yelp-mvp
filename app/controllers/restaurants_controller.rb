9d504ed4a4611f6420cf1e3c95f2f53e687b12fa
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-02-16 14:15:11 -0500

    refactored elements and improved app design

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,17 +6,34 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to(restaurant_path(@restaurant))
+    else
+      render :new
+    end
   end
 
   def show
+    @title =
+      if @restaurant.reviews.empty?
+        'No reviews yet!'
+      else
+        "All #{@restaurant.name} reviews:"
+      end
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :rating, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], message: "must be a number between 0 and 5"  }
   validates :rating, numericality: { only_integer: true }
   validates :content, presence: true
 end
